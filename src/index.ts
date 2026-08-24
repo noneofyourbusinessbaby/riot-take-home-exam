d34fcd55915ae0e77d342ca7df8835961955c873
noneofyourbusinessbaby <nadine.hajj-hassan@insa-lyon.fr>
2026-08-25 01:51:09 +0200

    feat: set up OpenAPI documentation

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.doc("/doc", {
+	openapi: "3.0.0",
+	info: {
+		version: "1.0.0",
+		title: "Riot take home exercise",
+	},
 });
+app.get("/ui", swaggerUI({ url: "/doc" }));
+app.get("/health", (c) => c.text("OK"));
 
 serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  },
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
 );
